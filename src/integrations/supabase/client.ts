218046afa4a3d4083ac896cfe204f83e6417be75
Ryanlathotep <ryany207@gmail.com>
2026-05-25 00:44:32 -0400

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const supabaseUrl = "https://supabase.co";
+const supabaseUrl = "https://ijhisspxewnmriuewaxs.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqaGlzc3B4ZXdubXJpdWV3YXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzODE3MDMsImV4cCI6MjA4NDk1NzcwM30.X9LQKEOkhckY5d3P7HWvxvvqHKctQU9CuegZgW85_zI";
 
 
